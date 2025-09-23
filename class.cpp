abf05bc27e093ad1d4bb5ab51a3b667bb063e4dc
sakshi mane <manesakshishivaji2006@gmail.com>
2025-09-23 18:23:20 +0530

    Add files via upload

---
+++ class.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+class hero {
+    public:
+    int health;
+    char level;
+
+};
+int main() {
+    hero h1;
+     cout<<"health is:"<< h1.health<<endl;
+      cout<<"level is:"<< h1.level <<endl;
+
+    
+
+    
+}
